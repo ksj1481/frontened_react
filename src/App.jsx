bbd05627235e60b623a2add60801842e276693cc
ksj1481 <volvobaby0610@naver.com>
2026-05-13 19:42:28 +0900

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
 import './App.css'
 
-const scores = [
-  {이름: "John", 국어: 90, 영어: 90, 수학: 90, 과학:90 },
-  {이름: "Peter", 국어: 90, 영어: 90, 수학: 90, 과학:90 },
-  {이름: "Susan", 국어: 90, 영어: 90, 수학: 90, 과학:90 },
-  {이름: "Sue", 국어: 90, 영어: 90, 수학: 90, 과학:90 },
+const scores = [ 
+  {  이름: "John", 국어: 90, 영어: 90,  수학: 80, 과학: 90 },
+  {  이름: "Peter", 국어: 90, 영어: 90,  수학: 80, 과학: 90 },
+  {  이름: "Susan", 국어: 90, 영어: 90,  수학: 80, 과학: 90 },
+  {  이름: "Sue", 국어: 90, 영어: 90,  수학: 80, 과학: 90 },
 ]
 
-const sources = [0,1,2,3]
-tmp = []
-for (let item of source){
-  tmp.push(item*2)
-}
-
-sources.map(item=>(item*2))
 
 function App() {
-  
-
+ 
   return (
+
+    
     <table>
+    
       <tr>
-        <th>이름</th>
-        <th>국어</th>
-        <th>영어</th>
-        <th>수학</th>
-        <th>과학</th>
+        {
+          Object.keys(scores[0]).map(key=>(
+            <th>{key}</th>
+          ))
+        }
       </tr>
-      {[0, 1, 2, 3].map(idx=> {
-        return
-      })}
-      <tr>
-        <td>{scores[idx], 이름}</td>
-        <td>{scores[idx], 국어}</td>
-        <td>{scores[idx], 영어}</td>
-        <td>{scores[idx], 수학}</td>
-        <td>{scores[idx], 과학}</td>
-      </tr>
-      <tr>
-        <td>{scores[idx], 이름}</td>
-        <td>{scores[idx], 국어}</td>
-        <td>{scores[idx], 영어}</td>
-        <td>{scores[idx], 수학}</td>
-        <td>{scores[idx], 과학}</td>
-        </tr>
-      <tr>
-        <td>{scores[idx], 이름}</td>
-        <td>{scores[idx], 국어}</td>
-        <td>{scores[idx], 영어}</td>
-        <td>{scores[idx], 수학}</td>
-        <td>{scores[idx], 과학}</td>
-      </tr>
+      {scores.map(item => 
+            (     
+             <tr>
+              {
+              Object.values(item).map(
+                value => <td>{value}</td>
+              )
+              }
+            </tr>
+            )
+        )
+      }
+      
     </table>
   )
 }
